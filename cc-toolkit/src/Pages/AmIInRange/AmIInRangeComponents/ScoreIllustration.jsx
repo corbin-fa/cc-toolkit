50d81fb5ce47500eca9fefb643abec67cd2ae112
Corbin <corbin@fahey.associates>
2025-05-15 22:10:01 -0700

    Score Illustration for Score Below Range

--- cc-toolkit/src/Pages/AmIInRange/AmIInRangeComponents/ScoreIllustration.jsx
+++ cc-toolkit/src/Pages/AmIInRange/AmIInRangeComponents/ScoreIllustration.jsx
@@ -1,11 +1,39 @@
 function ScoreIllustration({inRange, sMin, sMax, studentScore}) {
     if (inRange) {
-        return <ScoreIllustrationInRange studentScore={studentScore} sMin={sMin} sMax={sMax} />;
+        return <ScoreIllustrationInRange 
+                studentScore={studentScore} 
+                sMin={sMin} 
+                sMax={sMax} 
+            />;
+    } else if (studentScore < sMin) {
+        return <ScoreIllustrationBelowRange 
+                    studentScore={studentScore}
+                    sMin={sMin}
+                    sMax={sMax}
+                />;
     }
 }
 
+function ScoreIllustrationBelowRange({sMin, sMax, studentScore}) {
+    let schoolRangeWidth = ((sMax - sMin) / (sMax-studentScore)) * 100;
+
+    return (
+        <>
+        <div class="score_illustration_below_range">
+            <div class="below_range_score_indicator"></div>
+            <div class="below_range_school_range" style={{width: `${schoolRangeWidth}%`}}>
+                <div className="below_range_score_illustration_line"></div>
+            </div>
+        </div>
+        <div className="below_range_score_labels" style={{width: `${schoolRangeWidth}%`}}>
+                <div style={{marginRight: "auto"}}>{sMin}</div>
+                <div style={{marginLeft: "auto"}}>{sMax}</div>
+        </div>
+        </>
+    );
+}
+
 function ScoreIllustrationInRange({sMin, sMax, studentScore}) {
-    console.log(sMin, sMax, studentScore);
     let studentScoreToPercent = ((studentScore - sMin) / (sMax - sMin)) * 100;
 
     return (
