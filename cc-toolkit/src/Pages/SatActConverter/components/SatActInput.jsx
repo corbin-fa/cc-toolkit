f0e76932e848aeac7cb86cd1d7b841f94130d0dc
Corbin <corbin@fahey.associates>
2025-05-18 10:20:14 -0700

    Finished SAT ACT Conversion Functionality

--- cc-toolkit/src/Pages/SatActConverter/components/SatActInput.jsx
+++ cc-toolkit/src/Pages/SatActConverter/components/SatActInput.jsx
@@ -1,6 +1,6 @@
 import "../SatActConverter.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Form from 'react-bootstrap/Form';
 
@@ -17,15 +17,65 @@ function validSat(sat) {
     return sat <= 1600 && sat >= 400;
 }
 
+function validAct(act) {
+    return act <= 36 && act >= 1;
+}
+
 function convertSatToAct(sat) {
     sat = Math.round(sat / 10) * 10;
 
-    // TODO: THIS DOESNT ACTUALLY WORK
-    if (!validSat(sat)) {
+    if (!validSat) {
         return;
-    } else {
-        return 36 - (40 - Math.ceil(sat / 40));
-    }   
+    }
+
+    // TODO: simplify this
+    switch (true) {
+        case sat >= 1450:
+            return 36 - (40 - Math.ceil(sat / 40));
+        case sat >= 1300:
+            return 32 - (48 - Math.ceil(sat / 30));
+        case sat >= 1260:
+            return 27;
+        case sat >= 1230:
+            return 26;
+        case sat >= 1200:
+            return 25;
+        case sat >= 1160:
+            return 24;
+        case sat >= 1130:
+            return 23;
+        case sat >= 1100:
+            return 22;
+        case sat >= 1060:
+            return 21;
+        case sat >= 1030:
+            return 20;
+        case sat >= 990:
+            return 19;
+        case sat >= 960:
+            return 18;
+        case sat >= 920:
+            return 17;
+        case sat >= 880:
+            return 16;
+        case sat >= 830:
+            return 15;
+        case sat >= 780:
+            return 14;
+        case sat >= 730:
+            return 13;
+        case sat >= 690:
+            return 12;
+        case sat >= 650:
+            return 11;
+        case sat >= 620:
+            return 10;
+        case sat >= 590:
+            return 9;
+        default:
+            console.error(sat + " was an unhandled sat score");
+            return;
+    }  
     // if (1570 <= sat && sat <= 1600) {
     //     return 36;
     // } else if (1530 <= sat ** sat <=1560) {
@@ -33,23 +83,88 @@ function convertSatToAct(sat) {
     // }
 }
 
+function convertActToSat(act) {
+    act = Number(act);
+
+    if (!validAct(act)) {
+        return;
+    }
+
+    // TODO: simplify this
+    switch (true) {
+        case act === 36:
+            return 1590;
+        case act === 35:
+            return 1540;
+        case act === 34:
+            return 1500;
+        case act === 33:
+            return 1460;
+        case act === 32:
+            return 1430;
+        case act === 31:
+            return 1400;
+        case act === 30:
+            return 1370;
+        case act === 29:
+            return 1340;
+        case act === 28:
+            return 1310;
+        case act === 27:
+            return 1280;
+        case act === 26:
+            return 1240;
+        case act === 25:
+            return 1210;
+        case act === 24:
+            return 1180;
+        case act === 23:
+            return 1140;
+        case act === 22:
+            return 1110;
+        case act === 21:
+            return 1080;
+        case act === 20:
+            return 1040;
+        case act === 19:
+            return 1010;
+        case act === 18:
+            return 970;
+        case act === 17:
+            return 930;
+        case act === 16:
+            return 890;
+        case act === 15:
+            return 850;
+        case act === 14:
+            return 800;
+        case act === 13:
+            return 760;
+        case act === 12:
+            return 710;
+        case act === 11:
+            return 670;
+        case act === 10:
+            return 630;
+        case act === 9:
+            return 590;
+        default:
+            return;
+    }
+}
+
 function SatActInput() {
     const [satScore, setSatScore] = useState("");
     const [actScore, setActScore] = useState("");
 
-    function updateSatScore(event) {
-        
-    }
-
-    function updateActScore(event) {
-        setActScore(event.target.value);
-        // TODO: change sat to converted score
-    }
+    useEffect(() => {
+        console.log(satScore, actScore);
+    });
 
     return (
         <>
         <div className="sat_act_input_container">
-            <Form.Label className="input_form_label" htmlFor="inputPassword5">SAT</Form.Label>
+            <Form.Label className="input_form_label">SAT</Form.Label>
             <Form.Control
                 type="number"
                 id="range"
@@ -57,23 +172,32 @@ function SatActInput() {
                 onChange={(e) => {
                     let sat = e.target.value;
                     setSatScore(sat);
-                    setActScore(convertSatToAct(sat));
+                    if (validSat(sat)) {
+                        setActScore(convertSatToAct(sat));
+                    } else {
+                        setActScore("");
+                    }
                 }}
                 onKeyUp={handleEnter}
                 />
-            <Form.Label className="input_form_label" htmlFor="inputPassword5">ACT</Form.Label>
+            <Form.Label className="input_form_label">ACT</Form.Label>
             <Form.Control
                 type="number"
                 id="range"
-                // placeholder= "Exam Score..."
                 value={actScore}
-                onChange={updateActScore}
+                onChange={(e) => {
+                    let act = e.target.value;
+                    setActScore(act);
+                    if (validAct(act)) { 
+                        setSatScore(convertActToSat(act));
+                    } else {
+                        setSatScore("");
+                    }
+                }}
                 onKeyUp={handleEnter}
-                // min = {examTypeBounds[examType].min}
-                // max = {examTypeBounds[examType].max}
                 />
         </div>
-        {satScore !== "" && (satScore < 400 || satScore > 1600) && (
+        {satScore !== "" && (satScore < 590 || satScore > 1600) && (
             <p style={{ color: "red" }}>SAT score must be between 400 and 1600</p>
         )}
         {actScore !== "" && (actScore < 1 || actScore > 36) && (
